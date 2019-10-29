ce26d18b514cbc9f667a44b9c5329594e437b267
Eleni Zoumpoulaki <elenizoump@hotmail.com>
2019-10-29 16:37:29 +0000

    solved iterations 1 to 3

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,15 +1,28 @@
 // ITERATION 1
 
-function updateSubtotal($product) {
-  // ...
-}
+function updateSubtotal($productNode) {
+  const $priceNode = $productNode.querySelector(".price span");
+  const $quantityNode = $productNode.querySelector(".quantity input");
+  const priceValue = Number($priceNode.innerText);
+  const quantityValue = $quantityNode.valueAsNumber;
 
-function calculateAll() {
-  // ITERATION 2
-  //...
-  // ITERATION 3
-  //...
-}
+  const subTotalValue = priceValue * quantityValue;
+  const $subtotalNode = $productNode.querySelector(".subtotal span");
+  $subtotalNode.innerText = subTotalValue;
+  return subTotalValue;
+};
+
+
+function calculateAll($product) {
+  const $allProducts = document.querySelectorAll(".product");
+  let totalValue = 0;
+  $allProducts.forEach($product => {
+    totalValue += updateSubtotal($product);
+  })
+  document.querySelector("#total-value span").innerText = totalValue;
+
+};
+
 
 const $calculateTrigger = document.getElementById('calculate');
 
